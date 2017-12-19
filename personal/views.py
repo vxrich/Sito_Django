8bf35f3486f7259717726719f9853ecff249ecaa
vxrich <riccardo.grespan6@gmail.com>
2017-12-19 11:57:15 +0100

    Modulo mail

--- personal/views.py
+++ personal/views.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+import django.middleware.csrf as csrf
 
 def index(request):
     return render(request, 'personal/home.html')
 
 def contact(request):
+    csrf.get_token(request)
     return render(request, 'personal/contacts.html')
 
 def gallery(request):
