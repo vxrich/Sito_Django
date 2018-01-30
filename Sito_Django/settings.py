2ab9e8c49f3f8cbc36e2f8980f6c3a7877605b2e
vxrich <vxrich@localhost.localdomain>
2018-01-30 22:28:12 +0100

    Aggiunti thumbnail ai link dei post, bisogna correggere i link della navbar in modo che si riadattino correttamente

--- Sito_Django/settings.py
+++ Sito_Django/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+#MEDIA_ROOT = '/blog/templates/cover/'
+#MEDIA_URL = '/static/'
 #STATIC_ROOT = "/home/riccardogrespan/Sito_Django/static/"
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static/'),
--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from PIL import Image
 
 class Post(models.Model):
     title = models.CharField(max_length=140)
     body = models.TextField()
     date = models.DateTimeField()
+    cover = models.ImageField(upload_to="static/cover/")
 
     def __str__(self):
         return self.title
